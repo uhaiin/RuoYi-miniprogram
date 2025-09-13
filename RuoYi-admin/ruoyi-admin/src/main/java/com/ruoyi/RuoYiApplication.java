5cc752db3d3e91bf2edb652865909427c3ff26f5
jiancai.zhong <jiancai.zhong@email.cn>
2025-09-13 23:56:21 +0800

    feat(init): 初始化 RuoYi 项目基础结构和配置
    - 创建项目目录结构和基础文件
    - 添加开发、生产、预发布环境配置
    - 实现 401 和 404 错误页面组件
    - 添加全局状态管理 (store)
    - 配置路由和权限控制
    - 实现基础工具类和注解
    - 配置 Druid 数据源
    - 添加 Spring Boot 应用配置- 实现精确定义浮点数运算工具类
    - 添加地址解析和 Ajax 结果封装类- 配置 Vue 项目基础结构和样式
    - 添加 Quartz任务调度抽象类

---
+++ RuoYi-admin/ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -0,0 +1,29 @@
+package com.ruoyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ *
+ * @author ruoyi
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class RuoYiApplication {
+    public static void main(String[] args) {
+        // System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(RuoYiApplication.class, args);
+        System.out.println("""
+                (♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ \s
+                 .-------.       ____     __       \s
+                 |  _ _   \\      \\   \\   /  /   \s
+                 | ( ' )  |       \\  _. /  '      \s
+                 |(_ o _) /        _( )_ .'        \s
+                 | (_,_).' __  ___(_ o _)'         \s
+                 |  |\\ \\  |  ||   |(_,_)'        \s
+                 |  | \\ `'   /|   `-'  /          \s
+                 |  |  \\    /  \\      /          \s
+                 ''-'   `'-'    `-..-'             \s""");
+    }
+}
