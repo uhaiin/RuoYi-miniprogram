2b7eae75f7ca5f68482b5c4c30ed53cb82e683a5
jiancai.zhong <jiancai.zhong@email.cn>
2025-09-15 22:19:14 +0800

    feat(common): 添加 API 响应封装类和相关枚举
    - 新增 ApiResponse 类，用于统一封装 API 请求的响应结果
    - 添加 ApiResponseEnum 枚举类，定义常见的 API 响应状态码- ApiResponse 类提供了成功和失败的静态工厂方法，方便创建响应对象
    - 该实现提高了 API响应的一致性和可维护性

---
+++ RuoYi-admin/ruoyi-common/src/main/java/com/ruoyi/common/enums/ApiResponseEnum.java
@@ -0,0 +1,36 @@
+package com.ruoyi.common.enums;
+
+/**
+ * ApiResponse code 枚举类
+ *
+ * @author Jiancai.zhong
+ * @since 2024-10-21 10:03:29
+ */
+public enum ApiResponseEnum {
+
+	/**
+	 * 成功
+	 */
+	SUCCESS(0, "success"),
+
+	/**
+	 * 失败
+	 */
+	FAILURE(-1, "failure");
+
+	/**
+	 * 请求结果代码，0 代表成功，-1 代表失败
+	 */
+	public final Integer code;
+
+	/**
+	 * 请求结果消息
+	 */
+	public final String message;
+
+	ApiResponseEnum(Integer code, String message) {
+		this.code = code;
+		this.message = message;
+	}
+
+}
---
+++ RuoYi-admin/ruoyi-common/src/main/java/com/ruoyi/common/web/ApiResponse.java
@@ -0,0 +1,141 @@
+package com.ruoyi.common.web;
+
+import com.ruoyi.common.enums.ApiResponseEnum;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * 请求结果封装
+ *
+ * @param <T> 泛型
+ * @author jiancai.zhong
+ */
+@Getter
+@Setter
+public class ApiResponse<T> implements Serializable {
+
+    /**
+     * 默认构造器，创建一个 ExampleClass 的实例。
+     */
+    public ApiResponse() {
+        // 构造器的具体实现
+    }
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 是否成功
+     */
+    protected boolean success = true;
+
+    /**
+     * 请求结果代码
+     */
+    private Integer code = ApiResponseEnum.SUCCESS.code;
+
+    /**
+     * 请求结果消息
+     */
+    private String message = ApiResponseEnum.SUCCESS.message;
+
+    /**
+     * 请求结果数据
+     */
+    private T data;
+
+    /**
+     * 请求时候的时间戳
+     */
+    private Long timestamp = System.currentTimeMillis();
+
+    /**
+     * 构建一个表示成功响应的ApiResponse对象 该方法用于在API调用成功时，创建并返回一个包含成功标志和数据的响应对象
+     *
+     * @param data 成功返回的数据，可以是任何类型
+     * @param <T>  泛型参数，表示返回数据的类型
+     * @return 返回一个ApiResponse对象，其中包含成功标志和返回数据
+     */
+    public static <T> ApiResponse<T> success(T data) {
+        // 创建一个新的ApiResponse对象
+        ApiResponse<T> apiResponse = new ApiResponse<>();
+        // 设置ApiResponse对象的数据
+        apiResponse.setData(data);
+        // 设置ApiResponse对象的成功标志为true
+        apiResponse.setSuccess(true);
+        // 返回填充了数据和成功标志的ApiResponse对象
+        return apiResponse;
+    }
+
+    /**
+     * 创建一个表示失败的API响应对象 此方法用于生成一个失败状态的API响应，可以包含任意类型的响应数据
+     *
+     * @param data 响应的数据，可以是任意类型
+     * @param <T>  泛型参数，表示响应数据的类型
+     * @return 返回一个表示失败状态的API响应对象
+     */
+    public static <T> ApiResponse<T> failure(T data) {
+        ApiResponse<T> apiResponse = new ApiResponse<>();
+        apiResponse.setData(data);
+        apiResponse.setCode(ApiResponseEnum.FAILURE.code);
+        apiResponse.setMessage(ApiResponseEnum.FAILURE.message);
+        apiResponse.setSuccess(false);
+        return apiResponse;
+    }
+
+    /**
+     * 创建一个表示成功的API响应对象
+     *
+     * @param message 成功消息，用于描述操作成功的原因
+     * @param <T>     泛型参数，表示API响应中可能包含的数据类型
+     * @return 返回一个ApiResponse对象，表示操作成功
+     * <p>
+     * 此方法用于生成一个表示成功操作的API响应实例它初始化一个ApiResponse对象， 设置成功消息，并将成功标志设置为true，然后返回这个ApiResponse对象
+     */
+    public static <T> ApiResponse<T> success(String message) {
+        ApiResponse<T> apiResponse = new ApiResponse<>();
+        apiResponse.setMessage(message);
+        apiResponse.setSuccess(true);
+        return apiResponse;
+    }
+
+    /**
+     * 创建一个表示成功的API响应对象
+     *
+     * @param <T> 泛型参数，表示API响应中可能包含的数据类型
+     * @return ApiResponse<T> 返回一个ApiResponse对象，表示操作成功
+     * <p>
+     * 此方法用于生成一个表示成功操作的API响应实例它初始化一个ApiResponse对象， 设置成功消息，并将成功标志设置为true，然后返回这个ApiResponse对象
+     * @author Jiancai.zhong
+     * @date 2025-05-08 02:03:51
+     */
+    public static <T> ApiResponse<T> ok() {
+        ApiResponse<T> apiResponse = new ApiResponse<>();
+        apiResponse.setMessage("ok");
+        apiResponse.setSuccess(true);
+        return apiResponse;
+    }
+
+    /**
+     * 创建一个表示失败的API响应对象
+     *
+     * @param message 错误消息，用于描述失败的原因
+     * @param <T>     泛型标记，表示响应数据的类型
+     * @return 返回一个表示失败的ApiResponse对象，包含相应的错误消息和状态码
+     */
+    public static <T> ApiResponse<T> failure(String message) {
+        ApiResponse<T> apiResponse = new ApiResponse<>();
+        apiResponse.setMessage(message);
+        apiResponse.setCode(ApiResponseEnum.FAILURE.code);
+        apiResponse.setSuccess(false);
+        return apiResponse;
+    }
+
+    @SuppressWarnings("unused")
+    private void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
