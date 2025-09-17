e7edddbc0401b804901c318e8653e5c3bd9ff353
jiancai.zhong <jiancai.zhong@email.cn>
2025-09-17 21:00:29 +0800

    refactor(index): 循环等待 token 加载首页数据
    - 在页面加载时检查是否有 token
    - 如果有 token，则直接请求首页数据
    - 如果没有 token，则每隔50ms 检查一次，直到有 token 为止
    - 引入 getStorage 方法用于获取本地存储的 token

--- RuoYi-wechat/miniprogram/pages/index/index.js
+++ RuoYi-wechat/miniprogram/pages/index/index.js
@@ -3,7 +3,8 @@ import {
 } from '@/api/index';
 // 导入本地存储 api
 import {
-  setStorage
+  setStorage,
+  getStorage
 } from '@/utils/storage';
 // 导入封装通用模块方法
 import {
@@ -13,13 +14,28 @@ Page({
   data: {},
   // 监听页面的加载
   onLoad() {
-    // 在页面加载以后，调用获取首页数据的方法
-    this.getIndexData()
+    // 页面加载后检查是否有token
+    this.checkTokenAndLoadData();
+  },
+
+  /**
+   * 检查是否有token，没有则循环等待，有则加载数据
+   */
+  checkTokenAndLoadData() {
+    const token = getStorage("token");
+    if (token) {
+      // 有token，直接请求数据
+      this.getIndexData();
+    } else {
+      // 使用setTimeout递归检查，每隔50ms检查一次
+      setTimeout(() => {
+        this.checkTokenAndLoadData();
+      }, 50);
+    }
   },
   // 获取首页数据
   async getIndexData() {
     const res = await reqIndexData()
     console.log(res)
-    // setStorage('token', data.token);
   },
 })
