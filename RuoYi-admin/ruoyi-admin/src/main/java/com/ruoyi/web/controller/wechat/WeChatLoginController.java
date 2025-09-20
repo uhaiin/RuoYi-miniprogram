a0d45a0b3fb00f2ead36a3362bff82752926bf4a
jiancai.zhong <jiancai.zhong@email.cn>
2025-09-20 14:33:34 +0800

    feat(wechat): 添加微信用户登录接口的Swagger注解支持
    为WeChatLoginController添加了@Tag和@Operation注解，用于描述API接口信息。
    
    同时为WeChatUser实体类添加了@Schema注解，增强接口文档可读性。引入springdoc-openapi-starter
    - webmvc-ui依赖以支持Swagger UI展示

--- RuoYi-admin/ruoyi-admin/src/main/java/com/ruoyi/web/controller/wechat/WeChatLoginController.java
+++ RuoYi-admin/ruoyi-admin/src/main/java/com/ruoyi/web/controller/wechat/WeChatLoginController.java
@@ -9,16 +9,18 @@ import com.ruoyi.system.service.ISysUserService;
 import com.ruoyi.wechat.domain.WeChatSessionResponse;
 import com.ruoyi.wechat.domain.WeChatUser;
 import com.ruoyi.wechat.service.WeChatAuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "微信用户信息管理")
 @Slf4j
 @RestController
 public class WeChatLoginController {
-
 
     @Resource
     private WeChatAuthService weChatAuthService;
@@ -30,6 +32,7 @@ public class WeChatLoginController {
     private ISysUserService sysUserService;
 
 
+    @Operation(summary = "微信用户登录")
     @GetMapping("/wechat/login/{code}")
     public ApiResponse<WeChatUser> login(@PathVariable String code) {
         WeChatUser wechatUser = new WeChatUser();
