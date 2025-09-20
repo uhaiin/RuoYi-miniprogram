2e79f5cd03a5947cec8bde46c6e67d9c91b9db8c
jiancai.zhong <jiancai.zhong@email.cn>
2025-09-20 20:05:05 +0800

    feat(pages): 添加 bot 和 explore 页面基础结构
    新增了 bot 和 explore 两个页面的基础文件，包括 JS、JSON、SCSS 和 WXML 文件，
    并初始化了页面生命周期函数和基本配置。

--- RuoYi-wechat/miniprogram/pages/mine/mine.js
+++ RuoYi-wechat/miniprogram/pages/mine/mine.js
@@ -9,13 +9,75 @@ import {
 } from '@/utils/extendApi'
 
 ComponentWithStore({
-  data: {},
+  // 页面的初始数据
+  data: {
+    // 初始化第二个面板数据
+    initpanel: [{
+        url: '/pages/code/code',
+        title: '取码记录',
+        iconfont: 'icon-dingdan'
+      },
+      {
+        url: '/pages/password/password',
+        title: '修改密码',
+        iconfont: 'icon-dingdan'
+      },
+      {
+        url: '/pages/order_no/order_no',
+        title: '单号查询',
+        iconfont: 'icon-lipinka'
+      },
+      {
+        url: '/pages/code_no/code_no',
+        title: '卡密查询',
+        iconfont: 'icon-tuikuan'
+      }
+    ],
+    initAdminPanel: [{
+        url: '/pages/batch_import/batch_import',
+        title: '批量导入',
+        iconfont: 'icon-lipinka'
+      },
+      {
+        url: '/pages/inventory/inventory',
+        title: '库存统计',
+        iconfont: 'icon-tuikuan'
+      }
+    ],
+    initSuPanel: [{
+      url: '/pages/employee/employee',
+      title: '客服管理',
+      iconfont: 'icon-tuikuan'
+    }]
+  },
 
   storeBindings: {
     store: userStore,
-    fields: ['token', 'userInfo']
+    fields: ['token', 'userInfo', 'admin']
   },
 
-  methods: {},
+  methods: {
+    // 跳转到登录页面
+    toLoginPage() {
+      wx.navigateTo({
+        url: '/pages/login/login'
+      })
+    },
+    exit() {
+      // 清除所有缓存
+      wx.clearStorageSync()
+
+      // 清除初始值
+      this.setData({
+        token: "",
+        admin: "",
+        userInfo: {}
+      })
+
+      toast({
+        title: "成功退出"
+      })
+    }
+  },
 
 })
